05ecfed58107a4d3f85d6d249a09ee82a53d15b9
vadym <vadym.polyanski@gmail.com>
2018-01-30 23:18:02 +0200

    created events

---
+++ src/main/scala/com.polianskyi.server/Event.scala
@@ -0,0 +1,8 @@
+package com.polianskyi.server
+
+sealed trait Event
+case class Login(user: String) extends Event
+case class Logout(user: String) extends Event
+case class GetChatLog(from: String) extends Event
+case class ChatLog(log: List[String]) extends Event
+case class ChatMessage(from: String, message: String) extends Event
