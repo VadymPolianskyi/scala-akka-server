d97633f4ecd56d91d17406f0a8e6824fc9096622
vadym <vadym.polyanski@gmail.com>
2018-02-13 17:07:36 +0200

    created trait with connection

---
+++ src/main/scala/com/polianskyi/ahr/repository/ConcreteRedis.scala
@@ -0,0 +1,10 @@
+package com.polianskyi.ahr.repository
+
+import java.net.URI
+
+import scala.util.Try
+
+trait ConcreteRedis {
+  val redisUrl = URI.create("http://127.0.0.1:6379")
+  val pwd = Try(redisUrl.getUserInfo.split(":")(1)).toOption
+}
