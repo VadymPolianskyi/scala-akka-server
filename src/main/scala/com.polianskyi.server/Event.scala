cab21dbc9322936050a78e99a89c4d7cb406dc78
vadym <vadym.polyanski@gmail.com>
2018-02-13 16:45:52 +0200

    removed redundant files

--- src/main/scala/com.polianskyi.server/Event.scala
+++
@@ -1,8 +0,0 @@
-package com.polianskyi.server
-
-sealed trait Event
-case class Login(user: String) extends Event
-case class Logout(user: String) extends Event
-case class GetChatLog(from: String) extends Event
-case class ChatLog(log: List[String]) extends Event
-case class ChatMessage(from: String, message: String) extends Event
