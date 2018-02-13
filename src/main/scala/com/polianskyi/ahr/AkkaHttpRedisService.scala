14e468f8726636a6874ae16ab8aa7f838fbb85ac
vadym <vadym.polyanski@gmail.com>
2018-02-13 17:57:12 +0200

    added authentication method

--- src/main/scala/com/polianskyi/ahr/AkkaHttpRedisService.scala
+++ src/main/scala/com/polianskyi/ahr/AkkaHttpRedisService.scala
@@ -1,14 +1,20 @@
 package com.polianskyi.ahr
 
+import java.util.concurrent.TimeUnit
+
 import akka.actor.ActorRef
 import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
 import akka.http.scaladsl.model.StatusCodes.{InternalServerError, OK}
 import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.server.Route
+import akka.http.scaladsl.server.directives.Credentials
 import akka.pattern.ask
+import akka.util.Timeout
 import com.polianskyi.ahr.model.UserHandler
-import com.polianskyi.ahr.model.UserHandler.{User, UserDeleted, UserNotFound}
+import com.polianskyi.ahr.model.UserHandler.{GetUser, User, UserDeleted, UserNotFound}
 import spray.json.DefaultJsonProtocol
+
+import scala.concurrent.Future
 
 case class UserPwd(pwd: String)
 case class UpsertRequest(username: String, password: String)
@@ -22,8 +28,31 @@ trait Protocols extends DefaultJsonProtocol {
 }
 
 trait Service extends Protocols {
+
+  implicit def requestTimeout: Timeout = Timeout(5, TimeUnit.SECONDS)
+
   def userHandler: ActorRef
 
+  def userAuthenticate(credentials: Credentials): Future[Option[UserPwd]] = {
+    credentials match {
+      case p@Credentials.Provided(userName) =>
+        fetchUserId(userName).map {
+          case Some(UserPwd(id)) if p.verify(id) => Some(UserPwd(id))
+          case _ => None
+        }
+      case _ => Future.successful(None)
+    }
+  }
+
+  def fetchUserId(userName: String): Future[Option[UserPwd]] ={
+    (userHandler ? GetUser(userName)).map {
+      case User(_, p) => Some(UserPwd(p))
+      case _ => None
+    }
+  }
+
+
+//  registration
   val unsecuredRoutes: Route = {
     pathPrefix("api") {
       pathPrefix("user") {
