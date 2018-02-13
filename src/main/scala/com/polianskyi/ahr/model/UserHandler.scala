d13a21018ae68f5b781f92d99f38e4526dde75c1
vadym <vadym.polyanski@gmail.com>
2018-02-13 17:21:40 +0200

    added crud http handling

--- src/main/scala/com/polianskyi/ahr/model/UserHandler.scala
+++ src/main/scala/com/polianskyi/ahr/model/UserHandler.scala
@@ -1,6 +1,15 @@
 package com.polianskyi.ahr.model
 
+import akka.actor.{Actor, ActorLogging, Props}
+import com.polianskyi.ahr.repository.Repo
+import akka.pattern.pipe
+
+import scala.concurrent.ExecutionContextExecutor
+
+
 object UserHandler {
+  def props(db: Repo): Props = Props(new UserHandler(db))
+
   case class User(username: String, details: String)
   case class Register(username: String, password: String)
   case class Update(username: String, details: String)
@@ -10,4 +19,25 @@ object UserHandler {
   case class UserDeleted(username: String)
 }
 
+class UserHandler(db: Repo) extends Actor with ActorLogging{
+  import UserHandler._
+  implicit val ec: ExecutionContextExecutor = context.dispatcher
+  override def receive: Receive = {
+    case Register(id, pwd) => db.upsert(id, pwd) pipeTo sender()
 
+    case Update(id, details) => db.upsert(id, details) pipeTo sender()
+
+    case GetUser(uname) =>
+      val _sender = sender()
+      db.get(uname).foreach {
+        case Some(i) => _sender ! User(uname, i)
+        case None => _sender ! UserNotFound
+      }
+
+    case DeleteUser(uname) =>
+      val _sender = sender()
+      db.del(uname).foreach {
+        case i if i > 0 => _sender ! UserDeleted(uname)
+      }
+  }
+}
